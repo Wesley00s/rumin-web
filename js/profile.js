19373f4c9ec84053e6c761ddf4dee9e4c89e32e8
Wesley R. S. <130418293+Wesley00s@users.noreply.github.com>
2024-09-15 00:58:46 -0300

    Implementação de loading na página de perfil

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,19 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const userEmail = sessionStorage.getItem('userEmail');
+
+    function showLoadingSpinner() {
+        document.getElementById('loadingSpinner').style.display = 'flex';
+        document.querySelector('.spinner').style.display = 'block';
+
+    }
+
+    function hideLoadingSpinner() {
+        document.querySelector('#loadingSpinner').style.display = 'none';
+        document.querySelector('.spinner').style.display = 'none';
+    }
+
     async function fetchPropriedadeData() {
+        showLoadingSpinner();
         try {
             const response = await fetch('https://ruminweb-api-repo-v1-wvlj.onrender.com/user/' + userEmail);
             const data = await response.json();
@@ -15,9 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         } catch (error) {
             console.error("Erro ao buscar os dados: ", error);
+        } finally {
+            hideLoadingSpinner();
         }
     }
 
     fetchPropriedadeData();
 });
-
